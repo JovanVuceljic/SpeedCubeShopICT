e231a97645a618748dd790dadb570d752fcdbf50
Coja <jovan.vuceljic@digitalmind.rs>
2021-06-17 06:36:17 +0200

    JWT Token and Auth

---
+++ Scs.Api/Core/JwtManager.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Scs.DataAccess;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Rolled back JwtBearer to v3.14 
+ */
+
+namespace Scs.Api.Core
+{
+    public class JwtManager
+    {
+        private readonly ScsContext _context;
+
+        public JwtManager(ScsContext context)
+        {
+            _context = context;
+        }
+
+        public string MakeToken(string username, string password)
+        {
+            
+            var user = _context.Users.Include(u => u.UserUserCases)
+               .FirstOrDefault(x => x.Username == username && x.Password == password);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            var actor = new JwtActor
+            {
+                Id = user.Id,
+                AllowedUseCases = user.UserUserCases.Select(x => x.UseCaseId),
+                Identity = user.Username
+            };
+            
+            //var actor = new AdminFakeActor();
+
+            var issuer = "asp_api";
+            var secretKey = "ThisIsMyVerySecretKey";
+            var claims = new List<Claim> 
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString(), ClaimValueTypes.String, issuer),
+                new Claim(JwtRegisteredClaimNames.Iss, "asp_api", ClaimValueTypes.String, issuer),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64, issuer),
+                new Claim("UserId", actor.Id.ToString(), ClaimValueTypes.String, issuer),
+                new Claim("ActorData", JsonConvert.SerializeObject(actor), ClaimValueTypes.String, issuer)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var now = DateTime.UtcNow;
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: "Any",
+                claims: claims,
+                notBefore: now,
+                expires: now.AddSeconds(30),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- Scs.Api/Startup.cs
+++ Scs.Api/Startup.cs
@@ -1,24 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Scs.Api.Core;
 using Scs.Application;
 using Scs.Application.Commands;
 using Scs.Application.Queries;
 using Scs.DataAccess;
-using Scs.Implementation;
 using Scs.Implementation.Commands;
 using Scs.Implementation.Logging;
 using Scs.Implementation.Queries;
 using Scs.Implementation.Validators;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Scs.Implementation;
 
 namespace Scs.Api
 {
@@ -31,22 +36,68 @@ namespace Scs.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<ScsContext>();
-
-        /* Brand */
-            //services.AddTransient<ICreateBrandCommand, RawSqlCreateBrandCommand>();
             services.AddTransient<ICreateBrandCommand, EfCreateBrandCommad>();
             services.AddTransient<IDeleteBrandCommand, EfDeleteBrandCommand>();
             services.AddTransient<IGetBrandsQuery, EfGetBrandsQuery>();
-            services.AddTransient<CreateBrandValidator>();
+            services.AddHttpContextAccessor();
 
+            //services.AddTransient<IApplicationActor, AdminFakeActor>();
+            services.AddTransient<IApplicationActor>(x =>
+            {
+                var accessor = x.GetService<IHttpContextAccessor>();
+                Console.WriteLine(accessor);
+                var user = accessor.HttpContext.User;
 
-            services.AddTransient<IApplicationActor, AdminFakeActor>();
+                Console.WriteLine(user);
+                if (user.FindFirst("ActorData") == null)
+                {
+
+                    Console.WriteLine("InvalidOperationException");
+                    throw new InvalidOperationException("Actor data doesnt exist in token.");
+                }
+
+                var actorString = user.FindFirst("ActorData").Value;
+
+                Console.WriteLine(actorString);
+                var actor = JsonConvert.DeserializeObject<JwtActor>(actorString);
+
+                Console.WriteLine(actor);
+                return actor;
+
+            }); 
             services.AddTransient<IUseCaseLogger, ConsoleUseCaseLogger>();
             services.AddTransient<UseCaseExecutor>();
+            services.AddTransient<CreateBrandValidator>();
+            services.AddTransient<JwtManager>();
+
+
+
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(cfg =>
+            {
+                cfg.RequireHttpsMetadata = false;
+                cfg.SaveToken = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = "asp_api",
+                    ValidateIssuer = true,
+                    ValidAudience = "Any",
+                    ValidateAudience = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ThisIsMyVerySecretKey")),
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
             services.AddControllers();
         }
 
@@ -58,8 +109,18 @@ namespace Scs.Api
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(x =>
+            {
+                x.AllowAnyOrigin();
+                x.AllowAnyMethod();
+                x.AllowAnyHeader();
+            });
+
             app.UseRouting();
 
+            app.UseMiddleware<GlobalExceptionHandler>();
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
